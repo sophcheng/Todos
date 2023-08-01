af31006be563205f23db32fea4003ef7858ac7b3
Sophie Cheng <sophiecheng181@gmail.com>
2023-08-01 09:29:20 -0400

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Scholar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
